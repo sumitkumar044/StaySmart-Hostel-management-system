5fe2660b4138460882bdd7ba21242a57e83375b7
isuumitt <kumarsumit48338@gmail.com>
2026-01-02 05:05:35 +0530

    first commit

--- backend/src/routes/adminAuthRoutes.js
+++ backend/src/routes/adminAuthRoutes.js
@@ -1,12 +1,8 @@
 import express from "express";
-import {
-  sendAdminOtp,
-  verifyAdminOtp,
-} from "../controllers/adminAuthController.js";
+import { adminLogin } from "../controllers/adminAuthController.js";
 
 const router = express.Router();
 
-router.post("/send-otp", sendAdminOtp);
-router.post("/verify-otp", verifyAdminOtp);
+router.post("/login", adminLogin);
 
 export default router;
