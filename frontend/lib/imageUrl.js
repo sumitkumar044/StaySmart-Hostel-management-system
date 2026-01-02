8a248cc94a04726c232e25bacaea755f3a461620
isuumitt <kumarsumit48338@gmail.com>
2026-01-02 05:49:36 +0530

    register page and imageurl changes

--- frontend/lib/imageUrl.js
+++ frontend/lib/imageUrl.js
@@ -1,6 +1,7 @@
 // client/lib/imageUrl.js
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 export const imageUrl = (path) => {
   if (!path) return "";
   if (path.startsWith("http")) return path;
-  return `http://localhost:5000${path}`;
+  return `${BASE_URL}${path}`;
 };
